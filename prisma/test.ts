2b989c6509ab672f601d946c10a97c460cb383e2
Harry Hocker <harry_hocker@outlook.com>
2023-02-20 22:26:13 -0500

    Created "test.ts" file to test the functionality of Prisma.

---
+++ prisma/test.ts
@@ -0,0 +1,52 @@
+import {Customer, PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function queryAllCustomers() {
+    const allUsers = await prisma.customer.findMany()
+    console.log(allUsers)
+}
+
+async function createTestCustomer(customer: Customer) {
+    await prisma.customer.create({
+        data: {
+            first_name: customer.first_name,
+            middle_name: customer.middle_name,
+            last_name: customer.last_name,
+            address: customer.address,
+            email: customer.email,
+        }
+    })
+}
+
+let testCustomer: Customer = {
+    address: "this place is not real",
+    email: null,
+    first_name: "person name",
+    id: 0,
+    last_name: "last name",
+    middle_name: null,
+    login_id: null
+}
+
+createTestCustomer(testCustomer)
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
+
+queryAllCustomers()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
+
