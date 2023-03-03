60384242e0ac08333bd7cdc0ac4530d2880cd682
Harry Hocker <harry_hocker@outlook.com>
2023-03-02 23:23:09 -0500

    Created prisma database function for creating a type of Form. Added "request" to Form schema.

--- prisma/db/Form.ts
+++ prisma/db/Form.ts
@@ -1,10 +1,32 @@
-import {Form, PrismaClient} from "@prisma/client";
+import {Form, Parameter, Prisma, PrismaClient} from "@prisma/client";
 const prisma = new PrismaClient()
 
+// possible CRUD structure: https://www.prisma.io/docs/concepts/components/prisma-client/crud
+
 // CREATE
+export async function createForm(form: Form, parameters: Parameter[]) {
+    await prisma.form.create({
+        data: {
+            customerId: form.customerId,
+            employeeId: form.employeeId,
+
+            parameters: {
+                connect: parameters
+            }
+        }
+    })
+}
 
 // READ
+export async function readForm(form: Form) {
+    var foundForm = await prisma.form.findUnique({
+        where: {
+            id: form.id
+        }
+    })
+}
 
 // UPDATE
+
 
 // DELETE
