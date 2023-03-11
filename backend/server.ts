7ebccfcd640ea9f7af5ce7668c3e8fbc2fa585d6
Harry Hocker <harry_hocker@outlook.com>
2023-03-10 21:31:38 -0500

    Addes "cors" package.

--- backend/server.ts
+++ backend/server.ts
@@ -1,16 +1,19 @@
-// Import Express
 import express from 'express';
-// import the microservices
+import cors from 'cors';
+
 import UserRouter from './UserService/UserRoutes';
 import DataRouter from './DataService/DataRoutes';
 
 // Initialize the express app
 const app: express.Application = express();
 
+// allow for web-browser usage
+app.use(cors)
 
 // use the microservices
 app.use('/api/user', UserRouter)
 app.use('/api', DataRouter)
+
 
 // Server startup
 app.listen(process.env.PORT, () => {
