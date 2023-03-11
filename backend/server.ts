82ff11a78473d97ef1715e7b69216ec51df97c1a
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2023-03-11 08:59:53 -0800

    format after merge

--- backend/server.ts
+++ backend/server.ts
@@ -1,19 +1,18 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import UserRouter from './UserService/UserRoutes';
-import DataRouter from './DataService/DataRoutes';
+import UserRouter from "./UserService/UserRoutes";
+import DataRouter from "./DataService/DataRoutes";
 
 // Initialize the express app
 const app: express.Application = express();
 
 // allow for web-browser usage
-app.use(cors)
+app.use(cors);
 
 // use the microservices
 app.use("/api/user", UserRouter);
 app.use("/api", DataRouter);
-
 
 // Server startup
 app.listen(process.env.PORT, () => {
