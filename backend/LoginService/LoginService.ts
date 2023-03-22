5ba48a792959e594654ba790cbe9a35aea65c50a
Harry Hocker <harry_hocker@outlook.com>
2023-03-22 19:36:11 -0400

    Modified login() function's bcrypt error handling.

--- backend/LoginService/LoginService.ts
+++ backend/LoginService/LoginService.ts
@@ -6,46 +6,40 @@ import { compare, hash } from "bcrypt";
 import { generateToken } from "../JWTService";
 
 // needed for the JWT secret
-require('dotenv').config()
+require("dotenv").config();
 
 export async function login(req: Request, res: Response) {
-
   const parsedLogin: Login = {
     id: 0,
     email: req.body.email,
     password: req.body.password,
     role: null,
-    userId: null
+    userId: null,
   };
 
   try {
     // using PRISMA db function
     const login = await loginDB.find(parsedLogin);
     if (login != null) {
-      console.log(`login found: ${login.email}`);
       // required to make sure the Promises are not null
       if (parsedLogin.password != null && login.password != null) {
-        const isCompared = await compare(parsedLogin.password, login.password);
-        if (isCompared) {
-          console.log("Sending token.");
-          // UPDATE: sends the login data as a generated token instead of a simple JSON
-          const secret: string = process.env.JWT_SECRET || "";
-          res.status(200).send(
-            {
-              "token" : generateToken(login, secret)
+        compare(parsedLogin.password, login.password, (error, data) => {
+          if (error) throw error;
+          if (data) {
+            return res.status(200).send({
+              token: generateToken(login, process.env.JWT_SECRET || ""),
             });
-          return;
-        } else {
-          res.status(401).send("Invalid login");
-        }
+          } else {
+            return res.status(401).json({ error: "Invalid login" });
+          }
+        });
       }
     } else {
-      res.status(401).send("Invalid login");
-      throw console.error(`Login with email: ${parsedLogin.email} not found.`);
+      return res.status(401).send({ error: "Login not found." });
     }
   } catch (error) {
-    console.log(error);
-    res.status(401).send(error);
+    res.status(401).send({ error: error });
+    throw error;
   }
 }
 
@@ -66,13 +60,12 @@ export async function find(req: Request, res: Response) {
 
 // this should be a private function that cannot be used by a front-end until future expansion
 export async function register(req: Request, res: Response) {
-
   let parsedLogin: Login = {
     id: 0,
     email: req.body.email,
     password: req.body.password,
     role: req.body.role,
-    userId: req.body.personId
+    userId: req.body.personId,
   };
 
   // though a role is required, having a string helps more than "null"
