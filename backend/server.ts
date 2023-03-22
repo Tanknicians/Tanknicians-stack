72fb3d86ecec0a367ee81bdce0ad44ee91b86789
Harry Hocker <harry_hocker@icloud.com>
2023-03-21 21:06:14 -0400

    Removed unused commented-out code.

--- backend/server.ts
+++ backend/server.ts
@@ -2,9 +2,6 @@ import express from "express";
 import cors from "cors";
 
 import LoginRouter from "./LoginService/LoginRoutes";
-/*
-import DataRouter from "./DataService/DataRoutes";
-*/
 
 // read the required .env file 
 require('dotenv').config()
@@ -12,19 +9,17 @@ require('dotenv').config()
 // Initialize the express app
 const app: express.Application = express();
 
-// allow for web-browser usage
 // Set up cors options
 const corsOptions = {
   origin: "http://localhost:3000",
   credentials: true
 }
+
+// allow for web-browser usage
 app.use(cors(corsOptions));
 
 // use the services and route them out
 app.use("/api/login", LoginRouter);
-/*
-app.use("/api", DataRouter);
-*/
 
 // Server startup
 app.listen(process.env.PORT, () => {
