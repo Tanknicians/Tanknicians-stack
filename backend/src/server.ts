c608f14b14e0ebb0746bbbb015047f909911d459
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2023-03-23 14:59:52 -0400

    initial trpc config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,21 @@
-import express from "express";
+import * as trpcExpress from "@trpc/server/adapters/express";
 import cors from "cors";
+import express from "express";
 
 import LoginRouter from "./LoginService/LoginRoutes";
+import { createContext, router } from "./trpc";
 
-// read the required .env file 
-require('dotenv').config()
+// read the required .env file
+require("dotenv").config();
 
 // Initialize the express app
-const app: express.Application = express();
+const app = express();
 
 // Set up cors options
 const corsOptions = {
   origin: "http://localhost:3000",
-  credentials: true
-}
+  credentials: true,
+};
 
 // allow for web-browser usage
 app.use(cors(corsOptions));
@@ -25,3 +27,16 @@ app.use("/api/login", LoginRouter);
 app.listen(process.env.PORT, () => {
   console.log(`TypeScript with Express http://localhost:${process.env.PORT}/`);
 });
+
+const appRouter = router({
+  login: loginRouter
+})
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+);
+app.listen(4000);
