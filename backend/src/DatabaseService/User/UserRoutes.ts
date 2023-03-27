1ed30573f23aa92f167fcf27b77f5c5efa618d1c
Harry Hocker <harry_hocker@icloud.com>
2023-03-26 21:25:23 -0400

    Implemented basic CRUD routes for each model.

--- backend/src/DatabaseService/User/UserRoutes.ts
+++ backend/src/DatabaseService/User/UserRoutes.ts
@@ -2,8 +2,24 @@ import express, { Request, Response } from "express";
 
 const UserRouter = express.Router();
 
-UserRouter.post("/find", async (req: Request, res: Response) => {
-    console.log("User.find invoked.");
-  });
+UserRouter.post("/create", async (req: Request, res: Response) => {
+  console.log("User.create invoked.");
+});
 
-export default UserRouter
+UserRouter.post("/find", async (req: Request, res: Response) => {
+  console.log("User.find invoked.");
+});
+
+UserRouter.post("/update", async (req: Request, res: Response) => {
+  console.log("User.update invoked.");
+});
+
+UserRouter.post("/delete", async (req: Request, res: Response) => {
+  console.log("User.delete invoked.");
+});
+
+UserRouter.post("/search", async (req: Request, res: Response) => {
+  console.log("User.search invoked.");
+});
+
+export default UserRouter;
