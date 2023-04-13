d5296b6e0cd615447d52bf212b96de699c28a713
Harry Hocker <harry_hocker@outlook.com>
2023-04-12 21:46:29 -0400

    Just for a future reference, changed SCRoutes.ts to RESTful format as an example for tRPC reconstruction.

--- backend/src/DatabaseService/ServiceCall/ServiceCallRoutes.ts
+++ backend/src/DatabaseService/ServiceCall/ServiceCallRoutes.ts
@@ -2,19 +2,19 @@ import express, { Request, Response } from "express";
 
 const ServiceCallRouter = express.Router();
 
-ServiceCallRouter.post("/create", async (req: Request, res: Response) => {
+ServiceCallRouter.post("/", async (req: Request, res: Response) => {
   console.log("ServiceCall.create invoked.");
 });
 
-ServiceCallRouter.post("/read", async (req: Request, res: Response) => {
+ServiceCallRouter.get("/", async (req: Request, res: Response) => {
   console.log("ServiceCall.read invoked.");
 });
 
-ServiceCallRouter.post("/update", async (req: Request, res: Response) => {
+ServiceCallRouter.put("/", async (req: Request, res: Response) => {
   console.log("ServiceCall.update invoked.");
 });
 
-ServiceCallRouter.post("/delete", async (req: Request, res: Response) => {
+ServiceCallRouter.delete("/", async (req: Request, res: Response) => {
   console.log("ServiceCall.delete invoked.");
 });
 
