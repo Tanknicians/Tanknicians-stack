df3f7e3660cdcbe0fa85d485feee5a32d381d434
Harry Hocker <harry_hocker@outlook.com>
2023-04-13 16:49:26 -0400

    This did not work as expected. Revoked to static.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ const app = express();
 
 // Set up cors options
 const corsOptions = {
-  origin: `http://localhost:${process.env.PORT}/`,
+  origin: 'http://localhost:3000',
   credentials: true,
 };
 
