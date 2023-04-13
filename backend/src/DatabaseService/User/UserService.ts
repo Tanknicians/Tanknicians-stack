a4ca3eb724d4b66dd35d87ecf494267ea76fea36
Harry Hocker <harry_hocker@outlook.com>
2023-04-12 22:35:22 -0400

    Began implementation of User for services in tRPC.

--- backend/src/DatabaseService/User/UserService.ts
+++ backend/src/DatabaseService/User/UserService.ts
@@ -1,29 +1,26 @@
-import { Request, Response } from "express";
-import * as UserDB from "../../../prisma/db/User";
+import { Request, Response } from 'express';
+import * as UserDB from '../../../prisma/db/User';
+import * as Prisma from '@prisma/client';
+import { TRPCError } from '@trpc/server';
 
-export async function read(req: Request, res: Response) {
-  const { id } = req.body;
-  const err: string = `User with id: ${id} not found.`;
-
-  const user = await UserDB.read(id);
-  if (!user) {
-    console.error(err);
-    res.json({ success: false, message: err });
+export async function read(id: number) {
+  try {
+    const user = await UserDB.read(id);
+    if (!user) {
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: `User with id: ${id} not found.`
+      });
+    }
+    return user;
+  } catch (e) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'An error occured during read',
+      cause: e
+    });
   }
-
-  res.send(user);
 }
 
-// Search requires a STRING and searches all columns of User
-export async function search(req: Request, res: Response) {
-  const { search } = req.body;
-  const err: string = `User with search: "${search}" not found.`;
-
-  const result = await UserDB.search(search);
-  if (!result) {
-    console.error(err);
-    res.json({ success: false, message: err });
-  }
-
-  res.send(result);
-}
+// Search requires any STRING and searches all columns
+export async function search(search: string) {}
