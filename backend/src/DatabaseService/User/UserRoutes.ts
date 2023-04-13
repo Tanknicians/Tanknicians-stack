af8a17967cb7cd4051bf893e4c9fbf02562256b4
Harry Hocker <harry_hocker@outlook.com>
2023-04-13 17:08:57 -0400

    Converted from express routes to tRPC routes.

--- backend/src/DatabaseService/User/UserRoutes.ts
+++ backend/src/DatabaseService/User/UserRoutes.ts
@@ -4,7 +4,7 @@ import * as UserService from './UserService';
 import * as Prisma from '@prisma/client';
 
 const createMutation = publicProcedure
-  .use(isRoleCurryMiddleware(['EMPLOYEE']))
+  .use(isRoleCurryMiddleware(['ADMIN']))
   .input(
     z.object({
       // data values
