33b51730892a8e8fe04c321e2b9a1d7cf1e1551d
Harry Hocker <harry_hocker@outlook.com>
2023-04-12 21:54:06 -0400

    Added register functionality as an endpoint.
    NOTE: Should have a check to make sure users cannot set themselves to "admin"

--- backend/src/LoginService/LoginRoutes.ts
+++ backend/src/LoginService/LoginRoutes.ts
@@ -1,18 +1,33 @@
 import { z } from "zod";
 import { router, publicProcedure, isRoleCurryMiddleware } from "../trpc";
 import * as LoginService from "./LoginService";
+import * as Prisma from "@prisma/client";
 
 
 const loginMutation = publicProcedure
-  .input(z.object({ email: z.string().email(), password: z.string() }))
+  .input(z.object({ 
+    email: z.string().email(), 
+    password: z.string() }))
   .mutation(async ({ input }) => {
     return await LoginService.login(input);
   });
 
-const readQuery = publicProcedure
-  .input(z.object({ email: z.string().email() }))
+  const readQuery = publicProcedure
+    .input(z.object({
+      email: z.string().email()
+    }))
+    .mutation(async ({input}) => {
+      return await LoginService.read(input)
+    })
+
+const registerMutation = publicProcedure
+  .input(z.object({
+    email: z.string().email(),
+    password: z.string(),
+    role: z.string()
+  }))
   .query(async ({ input }) => {
-    return await LoginService.read(input);
+    return await LoginService.register(input as Omit<Prisma.Login, "id">);
   });
 
 const adminMutation = publicProcedure
@@ -24,5 +39,6 @@ const adminMutation = publicProcedure
 export const loginRouter = router({
   login: loginMutation,
   read: readQuery,
+  register: registerMutation,
   admin: adminMutation,
 });
