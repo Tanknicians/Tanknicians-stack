9306802e283e1bdd640dcb6ca2bb849a88db27ab
Harry Hocker <harry_hocker@outlook.com>
2023-04-16 23:36:48 -0400

    Added loginRouter to database routes.

--- backend/src/DatabaseService/DatabaseRoutes.ts
+++ backend/src/DatabaseService/DatabaseRoutes.ts
@@ -1,11 +1,13 @@
 // required imports: Express and Prisma Database
 import { router } from "../trpc";
+import { loginRouter } from "./Login/LoginRoutes";
 import { serviceCallRouter } from "./ServiceCall/ServiceCallRoutes";
 import { tankMetaDataRouter } from "./TankMetadata/TankMetadataRoutes";
 import { userRouter } from "./User/UserRoutes";
 
 export const databaseRouter = router({
-  usr: userRouter,
+  lg: loginRouter,
   sc: serviceCallRouter,
   td: tankMetaDataRouter,
+  usr: userRouter,
 });
