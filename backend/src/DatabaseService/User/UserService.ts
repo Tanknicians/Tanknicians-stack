353ba936efb6785417c24ba69b809345ffea5a28
Harry Hocker <harry_hocker@outlook.com>
2023-04-16 23:29:34 -0400

    Added CRUDs functions to the database services.

--- backend/src/DatabaseService/User/UserService.ts
+++ backend/src/DatabaseService/User/UserService.ts
@@ -1,7 +1,19 @@
-import { Request, Response } from "express";
 import { userDB } from "../../../prisma/db/User";
 import * as Prisma from "@prisma/client";
 import { TRPCError } from "@trpc/server";
+
+
+export async function create(user: Omit<Prisma.User, 'id'>) {
+  try {
+    await userDB.create(user);
+  } catch (e) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An error occured during create.",
+      cause: e,
+    });
+  }
+}
 
 export async function read(id: number) {
   try {
@@ -22,5 +34,47 @@ export async function read(id: number) {
   }
 }
 
+export async function update(user: Prisma.User) {
+  try {
+    await userDB.update(user);
+  } catch (e) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An error occured during update.",
+      cause: e,
+    });
+  }
+}
+
+export async function deleteOne(id: number) {
+  try {
+    await userDB.deleteUser(id);
+  } catch (e) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An error occured during delete.",
+      cause: e,
+    });
+  }
+}
+
+
 // Search requires any STRING and searches all columns
-export async function search(search: string) {}
+export async function search(search: string) {
+  try {
+    const searchData = userDB.search(search)
+    if (!searchData) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: `No searchUser from search found.`,
+      });
+    }
+    return searchData;
+  } catch(e) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "An error occured during search.",
+      cause: e,
+    });
+  }
+}
