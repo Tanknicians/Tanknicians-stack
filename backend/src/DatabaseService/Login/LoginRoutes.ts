cb0235b389f0cb3b7acc42cf210338cfcab59967
Harry Hocker <khyrdantai@icloud.com>
2023-04-18 15:57:19 -0400

    Filled in comments with proper objects and functions for completion of the current LoginDB endpoint.

--- backend/src/DatabaseService/Login/LoginRoutes.ts
+++ backend/src/DatabaseService/Login/LoginRoutes.ts
@@ -1,50 +1,58 @@
-import { z } from "zod";
+import { number, z } from "zod";
 import { router, publicProcedure, isRoleCurryMiddleware } from "../../trpc";
-//import * as Login from './LoginService';
+import * as Login from './LoginService';
 import * as Prisma from "@prisma/client";
+
 
 const createMutation = publicProcedure
   .use(isRoleCurryMiddleware(["ADMIN"]))
   .input(
     z.object({
-      // data values
+      email: z.string().email(),
+      password: z.string(),
+      role: z.nativeEnum(Prisma.Role),
+      userId: z.number().positive().int()
     }),
   )
   .mutation(async ({ input }) => {
-    // service function
+    return await Login.create(input);
   });
 
 const readQuery = publicProcedure
   .use(isRoleCurryMiddleware(["ADMIN"]))
   .input(
     z.object({
-      // data values
+      email: z.string().email()
     }),
   )
   .query(async ({ input }) => {
-    // service function
+    return await Login.read(input.email);
   });
 
 const updateMutation = publicProcedure
   .use(isRoleCurryMiddleware(["ADMIN"]))
   .input(
     z.object({
-      // data values
+      id: z.number().positive().int(),
+      email: z.string().email(),
+      password: z.string(),
+      role: z.nativeEnum(Prisma.Role),
+      userId: z.number().positive().int()
     }),
   )
   .mutation(async ({ input }) => {
-    // service function
+    return await Login.update(input);
   });
 
 const deleteMutation = publicProcedure
   .use(isRoleCurryMiddleware(["ADMIN"]))
   .input(
     z.object({
-      // data values
+      id: z.number().positive().int()
     }),
   )
   .mutation(async ({ input }) => {
-    // service function
+    return await Login.deleteOne(input.id);
   });
 
 export const loginRouter = router({
