ace4869369217b6c812b9d6027fbd03d12784493
Harry Hocker <khyrdantai@icloud.com>
2023-04-19 01:14:52 -0400

    Removed unnecessary routes.

--- backend/src/Authentication/AuthRoutes.ts
+++ backend/src/Authentication/AuthRoutes.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { router, publicProcedure, isRoleCurryMiddleware } from '../trpc';
+import { router, publicProcedure } from '../trpc';
 import * as AuthService from './AuthService';
 import * as Prisma from '@prisma/client';
 
@@ -14,16 +14,6 @@ const loginMutation = publicProcedure
     return await AuthService.login(input);
   });
 
-const readQuery = publicProcedure
-  .input(
-    z.object({
-      email: z.string().email()
-    })
-  )
-  .mutation(async ({ input }) => {
-    return await AuthService.read(input);
-  });
-
 const registerMutation = publicProcedure
   .input(
     z.object({
@@ -36,15 +26,7 @@ const registerMutation = publicProcedure
     return await AuthService.register(input as Omit<Prisma.Login, 'id'>);
   });
 
-const adminMutation = publicProcedure
-  .use(isRoleCurryMiddleware(['ADMIN']))
-  .mutation(async () => {
-    return 'success!';
-  });
-
 export const authRouter = router({
   login: loginMutation,
-  read: readQuery,
   register: registerMutation,
-  admin: adminMutation
 });
