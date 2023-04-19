6eae9aff8407645bfb36caafa395b53073149ef2
Harry Hocker <khyrdantai@icloud.com>
2023-04-19 01:11:36 -0400

    Completed UserService.

--- backend/src/DatabaseService/User/UserRoutes.ts
+++ backend/src/DatabaseService/User/UserRoutes.ts
@@ -1,55 +1,47 @@
-import { z } from "zod";
-import { router, publicProcedure, isRoleCurryMiddleware } from "../../trpc";
-import * as UserService from "./UserService";
-import * as Prisma from "@prisma/client";
+import { z } from 'zod';
+import { router, publicProcedure, isRoleCurryMiddleware } from '../../trpc';
+import * as UserService from './UserService';
+
+const UserServiceObject = z.object({
+  id: z.number().int(),
+  firstName: z.string().nullable(),
+  middleName: z.string().nullable(),
+  lastName: z.string().nullable(),
+  address: z.string().nullable(),
+  phone: z.string().nullable()
+});
 
 const createMutation = publicProcedure
-  .use(isRoleCurryMiddleware(["ADMIN"]))
-  .input(
-    z.object({
-      // data values
-    }),
-  )
+  .use(isRoleCurryMiddleware(['ADMIN']))
+  .input(UserServiceObject.omit({ id: true }))
   .mutation(async ({ input }) => {
-    // service function
+    return await UserService.create(input);
   });
 
 const readQuery = publicProcedure
-  .use(isRoleCurryMiddleware(["ADMIN"]))
-  .input(
-    z.object({
-      // data values
-    }),
-  )
+  .use(isRoleCurryMiddleware(['ADMIN']))
+  .input(UserServiceObject.pick({ id: true }))
   .query(async ({ input }) => {
-    // service function
+    return await UserService.read(input.id);
   });
 
 const updateMutation = publicProcedure
-  .use(isRoleCurryMiddleware(["ADMIN"]))
-  .input(
-    z.object({
-      // data values
-    }),
-  )
+  .use(isRoleCurryMiddleware(['ADMIN']))
+  .input(UserServiceObject)
   .mutation(async ({ input }) => {
-    // service function
+    return await UserService.update(input);
   });
 
 const deleteMutation = publicProcedure
-  .use(isRoleCurryMiddleware(["ADMIN"]))
-  .input(
-    z.object({
-      // data values
-    }),
-  )
+  .use(isRoleCurryMiddleware(['ADMIN']))
+  .input(UserServiceObject.pick({ id: true }))
   .mutation(async ({ input }) => {
-    // service function
+    return await UserService.deleteOne(input.id);
   });
 
 export const userRouter = router({
   create: createMutation,
   read: readQuery,
   update: updateMutation,
-  delete: deleteMutation,
+  delete: deleteMutation
 });
