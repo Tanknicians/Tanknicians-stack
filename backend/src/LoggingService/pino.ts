e703f4d64a99442cac72801790a3966c6163e321
Harry Hocker <khyrdantai@icloud.com>
2023-04-19 21:26:23 -0400

    Created initial logger file for both back-end and HTTP.

---
+++ backend/src/LoggingService/pino.ts
@@ -0,0 +1,32 @@
+import { pino } from 'pino';
+import { pinoHttp } from 'pino-http';
+
+// used with the back-end server
+const logger = pino(
+  {
+    transport: {
+      target: 'pino-pretty' // pretty printing
+    }
+  },
+  pino.destination({
+    dest: './log-file', // omit for stdout
+    minLength: 4096, // Buffer before writing
+    sync: false // Asynchronous logging
+  })
+);
+
+// used with Express/tRPC/HTTP methods
+const httpLogger = pinoHttp(
+  {
+    transport: {
+      target: 'pino-pretty' // pretty printing
+    }
+  },
+  pino.destination({
+    dest: './http-log-file', // omit for stdout
+    minLength: 4096, // Buffer before writing
+    sync: false // Asynchronous logging
+  })
+);
+
+// NOTE: need to have a log rotation service on the Ubuntu server
