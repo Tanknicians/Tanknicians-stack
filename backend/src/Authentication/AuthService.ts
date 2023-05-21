646ba266917d6345c4a3037d1d28cc9792ceac0d
SlimyMonz <khyrdantai@icloud.com>
2023-05-21 15:41:31 -0400

    Updated function with refresh token functionality.

--- backend/src/Authentication/AuthService.ts
+++ backend/src/Authentication/AuthService.ts
@@ -1,9 +1,13 @@
 // in here we may or may not use axios
 import * as Prisma from "@prisma/client";
 import * as bcrypt from "bcrypt";
+
 import {generateJWT, authenticateJWT, generateRefreshToken} from "../TokenGenerator";
 import { loginDB } from "../../prisma/db/Login";
+import { refreshTokenDB } from "../../prisma/db/RefreshToken";
+
 import { TRPCError } from "@trpc/server";
+
 
 const jwtSecret = process.env.JWT_SECRET;
 const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
@@ -121,15 +125,22 @@ export async function refresh(email: string, refreshToken: string) {
   
   // Validate the refresh token (expiration, integrity)
   let tokenValidation = authenticateJWT(refreshToken, true)
+  // todo: implement proper error checking
+  if (!tokenValidation) return;
 
   // Find the login based on the provided email
-  let login = await loginDB.read(email)
-  if (!login) return;
-  
-  // Compare the token associated with the login
-  
+  let dbLoginPayload = await loginDB.read(email)
+  // todo: implement proper error checking
+  if (!dbLoginPayload) return;
 
-  // Generate a new access token
+  // Find the refresh token based on the login
+  let dbRefreshTokenPayload = await refreshTokenDB.read(dbLoginPayload.id);
+  // todo: implement proper error checking
+  if (!dbRefreshTokenPayload) return;
+  
+  // Compare the token associated with the login from the database
+  if (refreshToken != dbRefreshTokenPayload.refreshToken) return;
 
-  // Return the new access token 
+  // Generate and return a new access token
+  return generateRefreshToken(dbLoginPayload)
 }
