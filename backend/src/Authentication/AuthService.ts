0cf292f42285371741158cbf5e8de0cf06c6ed73
SlimyMonz <khyrdantai@icloud.com>
2023-05-21 22:45:30 -0400

    Added refresh token being created, but not sure what to do if it already exists on login.

--- backend/src/Authentication/AuthService.ts
+++ backend/src/Authentication/AuthService.ts
@@ -1,16 +1,16 @@
 // in here we may or may not use axios
-import * as Prisma from "@prisma/client";
-import * as bcrypt from "bcrypt";
+import * as Prisma from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 import {
   generateJWT,
   authenticateJWT,
-  generateRefreshToken,
-} from "../TokenGenerator";
-import { loginDB } from "../../prisma/db/Login";
-import { refreshTokenDB } from "../../prisma/db/RefreshToken";
+  generateRefreshToken
+} from '../TokenGenerator';
+import { loginDB } from '../../prisma/db/Login';
+import { refreshTokenDB } from '../../prisma/db/RefreshToken';
 
-import { TRPCError } from "@trpc/server";
+import { TRPCError } from '@trpc/server';
 
 export async function login(login: { email: string; password: string }) {
   const { email, password } = login;
@@ -21,14 +21,14 @@ export async function login(login: { email: string; password: string }) {
   // Confirm login credentials existed in full in DB
   if (!savedCredentials) {
     throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: `Login with email: ${email} not found.`,
+      code: 'UNAUTHORIZED',
+      message: `Login with email: ${email} not found.`
     });
   }
   if (!(savedCredentials.email && savedCredentials.password)) {
     throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: "User record incomplete",
+      code: 'UNAUTHORIZED',
+      message: 'User record incomplete'
     });
   }
 
@@ -42,51 +42,73 @@ export async function login(login: { email: string; password: string }) {
     bcrypt.compare(
       password,
       savedCredentials.password,
-      function (err, samePasswords) {
+      async function (err, samePasswords) {
         if (err) {
           reject(
             new TRPCError({
-              code: "INTERNAL_SERVER_ERROR",
-              message: "Error occured during password compare",
-              cause: err,
-            }),
+              code: 'INTERNAL_SERVER_ERROR',
+              message: 'Error occured during password compare',
+              cause: err
+            })
           );
           return;
         }
         if (!samePasswords) {
           reject(
             new TRPCError({
-              code: "CONFLICT",
-              message: "passwords do not match",
-            }),
+              code: 'CONFLICT',
+              message: 'passwords do not match'
+            })
           );
           return;
         }
         try {
           const token = generateJWT(savedCredentials);
           const refreshToken = generateRefreshToken(savedCredentials);
+          const refreshTokenPayload = await refreshTokenDB.read(
+            savedCredentials.id
+          );
+          try {
+            if (!refreshTokenPayload) {
+              refreshTokenDB.create({
+                refreshToken: refreshToken,
+                loginId: savedCredentials.id
+              });
+            } else {
+             // what should we do here?
+            }
+          } catch (err) {
+            console.error(err);
+            reject(
+              new TRPCError({
+                code: 'UNAUTHORIZED',
+                message: 'Cannot generate token for session',
+                cause: err
+              })
+            );
+          }
           resolve({ token, refreshToken, savedCredentials });
         } catch (err) {
           console.error(err);
           reject(
             new TRPCError({
-              code: "UNAUTHORIZED",
-              message: "Cannot generate token for session",
-              cause: err,
-            }),
+              code: 'UNAUTHORIZED',
+              message: 'Cannot generate token for session',
+              cause: err
+            })
           );
         }
-      },
+      }
     );
   });
 }
 
-export async function register(login: Omit<Prisma.Login, "id">) {
+export async function register(login: Omit<Prisma.Login, 'id'>) {
   // though a role is required, having a string helps more than "null"
   if (login.role == null) {
     throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Role cannot be empty.",
+      code: 'BAD_REQUEST',
+      message: 'Role cannot be empty.'
     });
   }
 
@@ -95,8 +117,8 @@ export async function register(login: Omit<Prisma.Login, "id">) {
     login.password = await bcrypt.hash(login.password, 10);
   } else {
     throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Password cannot be empty.",
+      code: 'BAD_REQUEST',
+      message: 'Password cannot be empty.'
     });
   }
 
@@ -104,9 +126,9 @@ export async function register(login: Omit<Prisma.Login, "id">) {
     await loginDB.create(login);
   } catch (error) {
     throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "An error occured during registration",
-      cause: error,
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'An error occured during registration',
+      cause: error
     });
   }
 }
