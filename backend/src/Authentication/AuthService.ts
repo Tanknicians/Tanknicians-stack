863675571f5d90f94b620864f8bf74e012873e06
SlimyMonz <khyrdantai@icloud.com>
2023-05-21 22:27:43 -0400

    Split generation of token and refresh token into separate functions. Removed redundant dependency of secrets.

--- backend/src/Authentication/AuthService.ts
+++ backend/src/Authentication/AuthService.ts
@@ -1,19 +1,16 @@
 // in here we may or may not use axios
-import * as Prisma from "@prisma/client";
-import * as bcrypt from "bcrypt";
+import * as Prisma from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 import {
   generateJWT,
   authenticateJWT,
-  generateRefreshToken,
-} from "../TokenGenerator";
-import { loginDB } from "../../prisma/db/Login";
-import { refreshTokenDB } from "../../prisma/db/RefreshToken";
+  generateRefreshToken
+} from '../TokenGenerator';
+import { loginDB } from '../../prisma/db/Login';
+import { refreshTokenDB } from '../../prisma/db/RefreshToken';
 
-import { TRPCError } from "@trpc/server";
-
-const jwtSecret = process.env.JWT_SECRET;
-const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
+import { TRPCError } from '@trpc/server';
 
 export async function login(login: { email: string; password: string }) {
   const { email, password } = login;
@@ -24,87 +21,72 @@ export async function login(login: { email: string; password: string }) {
   // Confirm login credentials existed in full in DB
   if (!savedCredentials) {
     throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: `Login with email: ${email} not found.`,
+      code: 'UNAUTHORIZED',
+      message: `Login with email: ${email} not found.`
     });
   }
   if (!(savedCredentials.email && savedCredentials.password)) {
     throw new TRPCError({
-      code: "UNAUTHORIZED",
-      message: "User record incomplete",
+      code: 'UNAUTHORIZED',
+      message: 'User record incomplete'
     });
   }
 
   console.log(`login found for ${email}`);
 
-  return new Promise<{ 
-    token: string; 
+  return new Promise<{
+    token: string;
     refreshToken: string;
-    savedCredentials: Prisma.Login }>(
-    (resolve, reject) => {
-      bcrypt.compare(
-        password,
-        savedCredentials.password,
-        function (err, samePasswords) {
-          if (err) {
-            reject(
-              new TRPCError({
-                code: "INTERNAL_SERVER_ERROR",
-                message: "Error occured during password compare",
-                cause: err,
-              }),
-            );
-            return;
-          }
-          if (!samePasswords) {
-            reject(
-              new TRPCError({
-                code: "CONFLICT",
-                message: "passwords do not match",
-              }),
-            );
-            return;
-          }
-          try {
-            if (!jwtSecret) {
-              reject(
-                new TRPCError({
-                  code: "PRECONDITION_FAILED",
-                  message: "JWT_SECRET is not set in environment variables.",
-                }),
-              );
-              return;
-            }
-            const token = generateJWT(
-              savedCredentials,
-              jwtSecret,
-            );
-            const refreshToken = generateRefreshToken(
-              savedCredentials,
-            );
-            resolve({ token, refreshToken, savedCredentials });
-          } catch (err) {
-            console.error(err);
-            reject(
-              new TRPCError({
-                code: "UNAUTHORIZED",
-                message: "Cannot generate token for session",
-                cause: err,
-              }),
-            );
-          }
-        },
-      );
-    },
-  );
+    savedCredentials: Prisma.Login;
+  }>((resolve, reject) => {
+    bcrypt.compare(
+      password,
+      savedCredentials.password,
+      function (err, samePasswords) {
+        if (err) {
+          reject(
+            new TRPCError({
+              code: 'INTERNAL_SERVER_ERROR',
+              message: 'Error occured during password compare',
+              cause: err
+            })
+          );
+          return;
+        }
+        if (!samePasswords) {
+          reject(
+            new TRPCError({
+              code: 'CONFLICT',
+              message: 'passwords do not match'
+            })
+          );
+          return;
+        }
+        try {
+          const token = generateJWT(savedCredentials);
+          const refreshToken = generateRefreshToken(savedCredentials);
+          resolve({ token, refreshToken, savedCredentials });
+        } catch (err) {
+          console.error(err);
+          reject(
+            new TRPCError({
+              code: 'UNAUTHORIZED',
+              message: 'Cannot generate token for session',
+              cause: err
+            })
+          );
+        }
+      }
+    );
+  });
 }
 
-export async function register(login: Omit<Prisma.Login, "id">) {
+export async function register(login: Omit<Prisma.Login, 'id'>) {
   // though a role is required, having a string helps more than "null"
   if (login.role == null) {
     throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Role cannot be empty.",
+      code: 'BAD_REQUEST',
+      message: 'Role cannot be empty.'
     });
   }
 
@@ -113,8 +95,8 @@ export async function register(login: Omit<Prisma.Login, "id">) {
     login.password = await bcrypt.hash(login.password, 10);
   } else {
     throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Password cannot be empty.",
+      code: 'BAD_REQUEST',
+      message: 'Password cannot be empty.'
     });
   }
 
@@ -122,18 +104,15 @@ export async function register(login: Omit<Prisma.Login, "id">) {
     await loginDB.create(login);
   } catch (error) {
     throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "An error occured during registration",
-      cause: error,
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'An error occured during registration',
+      cause: error
     });
   }
 }
 
 // Generate a new access token using a refresh token; update this function to include proper checks and error messaging
 export async function refresh(email: string, refreshToken: string) {
-
-  // make sure jwtSecret loaded in
-  if (!jwtSecret) return;
   // Validate the refresh token (expiration, integrity)
   const tokenValidation = authenticateJWT(refreshToken, true);
   // todo: implement proper error checking
@@ -153,5 +132,5 @@ export async function refresh(email: string, refreshToken: string) {
   if (refreshToken !== dbRefreshTokenPayload.refreshToken) return;
 
   // Generate and return a new access token
-  return generateJWT(dbLoginPayload, jwtSecret, true)
+  return generateJWT(dbLoginPayload);
 }
--- backend/src/TokenGenerator.ts
+++ backend/src/TokenGenerator.ts
@@ -1,40 +1,42 @@
-import { Login, Role } from "@prisma/client";
-import * as jwt from "jsonwebtoken";
-import { randomBytes } from "crypto";
-import { z } from "zod";
+import { Login, Role } from '@prisma/client';
+import * as jwt from 'jsonwebtoken';
+import { randomBytes } from 'crypto';
+import { z } from 'zod';
 
 const jwtSecret = process.env.JWT_SECRET;
 const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
 
 // Generates 256bit hex string for jwt secret
 export function generateSecret(): string {
-  return randomBytes(42).toString("hex");
+  return randomBytes(42).toString('hex');
 }
 
 // Signs a token with a secret
-export function generateJWT(
-  login: Login,
-  secret: string,
-  isRefreshToken = false,
-): string {
-  const expiresIn = isRefreshToken ? "7d" : "24h";
+export function generateJWT(login: Login): string {
+  if (!jwtSecret) throw new Error('JWT secret not found.');
+  const expiresIn = '24h';
   const payload = {
     data: login,
-    isRefreshToken,
+    isRefreshToken: false
   };
-  return jwt.sign(payload, secret, { expiresIn });
+  return jwt.sign(payload, jwtSecret, { expiresIn });
 }
 
 export function generateRefreshToken(login: Login): string {
-  if (!jwtRefreshSecret) throw new Error("Refresh secret not found");
-  return generateJWT(login, jwtRefreshSecret, true);
+  if (!jwtRefreshSecret) throw new Error('Refresh secret not found.');
+  const expiresIn = '7d';
+  const payload = {
+    data: login,
+    isRefreshToken: true
+  };
+  return jwt.sign(payload, jwtRefreshSecret, { expiresIn });
 }
 
 // middleware for JWT auth
 export function authenticateJWT(token?: string, isRefreshToken = false) {
   // change this to proper error checking
   if (!(token && jwtSecret && jwtRefreshSecret))
-    throw new Error("Secret not found!");
+    throw new Error('Secret not found!');
   const secret = isRefreshToken ? jwtRefreshSecret : jwtSecret;
   return verifyJWT(token, secret, isRefreshToken);
 }
@@ -45,7 +47,7 @@ const JwtPayload = z.object({
   email: z.string(),
   role: z.nativeEnum(Role),
   userId: z.number().nullable(),
-  isRefreshToken: z.boolean().optional(),
+  isRefreshToken: z.boolean().optional()
 });
 type JwtPayload = z.infer<typeof JwtPayload>;
 
@@ -53,11 +55,11 @@ type JwtPayload = z.infer<typeof JwtPayload>;
 function verifyJWT(
   token: string,
   secret: string,
-  isRefreshToken: boolean,
+  isRefreshToken: boolean
 ): JwtPayload {
   const payload = JwtPayload.parse(jwt.verify(token, secret));
   if (isRefreshToken && !payload.isRefreshToken) {
-    throw new Error("Invalid token type");
+    throw new Error('Invalid token type');
   }
   return payload;
 }
