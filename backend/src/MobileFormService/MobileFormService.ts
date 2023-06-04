53f0f311b93bf291c89c523aaf697616097abdcd
SlimyMonz <khyrdantai@icloud.com>
2023-06-04 12:46:54 -0400

    Started implementation of checkServiceCall with the idea in mind to use previous service calls and their averages to validate data.

--- backend/src/MobileFormService/MobileFormService.ts
+++ backend/src/MobileFormService/MobileFormService.ts
@@ -10,12 +10,63 @@ export async function uploadServiceCall(serviceCall: Omit<Prisma.ServiceCall, "i
 }
 
 // run checks on the service call and make sure parameters are valid
-export function checkServiceCall(serviceCall: Omit<Prisma.ServiceCall, "id">){
+export async function checkServiceCall(serviceCall: Omit<Prisma.ServiceCall, "id">) {
 
+    // No error checking needed if previous do not exist. New customers won't have previous service call forms.
+    const previousServiceCalls = await serviceCallDB.readLatest(serviceCall.tankId)
+
+    if (previousServiceCalls.length > 0) {
+        const i = previousServiceCalls.length;
+        const averages = {
+          alkalinity: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.alkalinity, 0) / i,
+          calcium: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.calcium, 0) / i,
+          nitrate: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.nitrate, 0) / i,
+          phosphate: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.phosphate, 0) / i
+        };
+      
+        if (Math.abs(averages.alkalinity - serviceCall.alkalinity) > delta.alkalinity
+          || Math.abs(averages.calcium - serviceCall.calcium) > delta.calcium
+          || Math.abs(averages.nitrate - serviceCall.nitrate) > delta.nitrate
+          || Math.abs(averages.phosphate - serviceCall.phosphate) > delta.phosphate) {
+          serviceCall.isApproved = false;
+        }
+      }
+      
+
+    /* 
     
+    a function to fetch a "previous" service call? 4
+            ie: the latest form uploaded on May 3rd, 2023
+        if there is no previous SC, skip comparisons
+            
 
+        the function could also be generating a log of which of the specified parameters are out of range ?
+
+        primary goal: compare to 'reasonable' min-max ranges and set the "isApproved" boolean to true/false
+
+            make sure EmployeeID and TankMetadataID exist both in the object and serverside, else it will be thrown into a void of non-relation and lost forever
+
+    */
 }
 
 
 // const for valid parameters
 
+// const for valid delta (0.1 delta off average okay? etc)
+
+const delta = {
+    alkalinity: 0.1,
+    calcium: 0.1,
+    nitrate: 0.1,
+    phosphate: 0.1,
+}
+
+/*
+min, max, and previous?
+
+  alkalinity Float
+  calcium    Float
+  nitrate    Float
+  phosphate  Float
+
+*/
