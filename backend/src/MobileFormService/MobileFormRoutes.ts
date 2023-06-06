c9ab11cef325d1da55748041213ad5c7a09d0525
SlimyMonz <khyrdantai@icloud.com>
2023-06-05 23:57:24 -0400

    Exposed mobile form api.

--- backend/src/MobileFormService/MobileFormRoutes.ts
+++ backend/src/MobileFormService/MobileFormRoutes.ts
@@ -1,10 +1,14 @@
 import { router, publicProcedure, isRoleCurryMiddleware } from 'trpc';
-import { ServiceCall } from "types";
+import { ServiceCall } from 'types';
+import { uploadServiceCall } from './MobileFormService';
 
 const uploadFormMutation = publicProcedure
-  .use(isRoleCurryMiddleware(["ADMIN", "EMPLOYEE"]))
+  .use(isRoleCurryMiddleware(['ADMIN', 'EMPLOYEE']))
   .input(ServiceCall.omit({ id: true }))
   .mutation(async ({ input }) => {
-    // function for doing checks on the service call
+    return await uploadServiceCall(input);
   });
 
+export const mobileFormRouter = router({
+  uploadForm: uploadFormMutation
+});
