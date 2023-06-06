4f0ff1df975fa2a50b6ce09b95c51cf3b1461182
SlimyMonz <khyrdantai@icloud.com>
2023-06-05 23:53:38 -0400

    Finished upload service call mobile endpoint function with standard deviation calculations.

--- backend/src/MobileFormService/MobileFormService.ts
+++ backend/src/MobileFormService/MobileFormService.ts
@@ -1,66 +1,122 @@
-import * as Prisma from "@prisma/client";
-import { serviceCallDB } from "prisma/db/ServiceCall";
-import { TRPCError } from "@trpc/server";
+import * as Prisma from '@prisma/client';
+import { serviceCallDB } from 'prisma/db/ServiceCall';
+import { TRPCError } from '@trpc/server';
 
-
-
-export async function uploadServiceCall(serviceCall: Omit<Prisma.ServiceCall, "id">) {
-  
-
+export async function uploadServiceCall(
+  serviceCall: Omit<Prisma.ServiceCall, 'id'>
+) {
+  const submitServiceCall = await checkServiceCall(serviceCall);
+  try {
+    await serviceCallDB.create(submitServiceCall);
+  } catch (e) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'An error occured during create.',
+      cause: e
+    });
+  }
 }
 
 // run checks on the service call and make sure parameters are valid
-export async function checkServiceCall(serviceCall: Omit<Prisma.ServiceCall, "id">) {
-
-    // No error checking needed if previous do not exist. New customers won't have previous service call forms.
-    const previousServiceCalls = await serviceCallDB.readLatest(serviceCall.tankId)
-
-    if (previousServiceCalls.length > 0) {
-        const i = previousServiceCalls.length;
-        const averages = {
-          alkalinity: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.alkalinity, 0) / i,
-          calcium: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.calcium, 0) / i,
-          nitrate: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.nitrate, 0) / i,
-          phosphate: previousServiceCalls.reduce((total, serviceCall) => total + serviceCall.phosphate, 0) / i
-        };
-      
-        if (Math.abs(averages.alkalinity - serviceCall.alkalinity) > delta.alkalinity
-          || Math.abs(averages.calcium - serviceCall.calcium) > delta.calcium
-          || Math.abs(averages.nitrate - serviceCall.nitrate) > delta.nitrate
-          || Math.abs(averages.phosphate - serviceCall.phosphate) > delta.phosphate) {
-          serviceCall.isApproved = false;
-        }
-      }
-      
-
-    /* 
-        the function could also be generating a log of which of the specified parameters are out of range ?
-
-        primary goal: compare to 'reasonable' min-max ranges and set the "isApproved" boolean to true/false
-
-            make sure EmployeeID and TankMetadataID exist both in the object and serverside, else it will be thrown into a void of non-relation and lost forever
-
-    */
+export async function checkServiceCall(
+  serviceCall: Omit<Prisma.ServiceCall, 'id'>
+) {
+  // No error checking needed if previous do not exist. New customers won't have previous service call forms.
+  const previousServiceCalls = await serviceCallDB.readLatest(
+    serviceCall.tankId
+  );
+  // need at least 2 service calls to find deltas
+  if (previousServiceCalls.length > 1) {
+    const i = previousServiceCalls.length;
+    const averages = {
+      alkalinity:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.alkalinity,
+          0
+        ) / i,
+      calcium:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.calcium,
+          0
+        ) / i,
+      nitrate:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.nitrate,
+          0
+        ) / i,
+      phosphate:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.phosphate,
+          0
+        ) / i
+    };
+    // calculate standard deviation of previous service calls
+    const standardDeviation = calculateStandardDeviation(previousServiceCalls);
+    // if there are any deltas that are greater than the standard deviation, flag the service call
+    if (
+      Math.abs(averages.alkalinity - serviceCall.alkalinity) >
+        standardDeviation.alkalinity ||
+      Math.abs(averages.calcium - serviceCall.calcium) >
+        standardDeviation.calcium ||
+      Math.abs(averages.nitrate - serviceCall.nitrate) >
+        standardDeviation.nitrate ||
+      Math.abs(averages.phosphate - serviceCall.phosphate) >
+        standardDeviation.phosphate
+    ) {
+      serviceCall.isApproved = false;
+    }
+  }
+  // return the flagged/unflagged service call
+  return serviceCall;
 }
 
+// Standard deviation calculations, takes in an array of Service Calls without "id"
+function calculateStandardDeviation(data: Omit<Prisma.ServiceCall, 'id'>[]): {
+  nitrate: number;
+  phosphate: number;
+  calcium: number;
+  alkalinity: number;
+} {
+  // Calculate the mean values for nitrate, phosphate, calcium, and alkalinity
+  const meanValues = data.reduce(
+    (params, current) => {
+      params.alkalinity += current.alkalinity;
+      params.calcium += current.calcium;
+      params.nitrate += current.nitrate;
+      params.phosphate += current.phosphate;
+      return params;
+    },
+    { alkalinity: 0, calcium: 0, nitrate: 0, phosphate: 0 }
+  );
 
-// const for valid parameters
+  meanValues.nitrate /= data.length;
+  meanValues.phosphate /= data.length;
+  meanValues.calcium /= data.length;
+  meanValues.alkalinity /= data.length;
 
-// const for valid delta (0.1 delta off average okay? etc)
-// this needs to be updated for actual proper deltas 
-const delta = {
-    alkalinity: 0.1,
-    calcium: 0.1,
-    nitrate: 0.1,
-    phosphate: 0.1,
+  // Calculate the sum of squared differences from the mean
+  const squaredDifferences = data.reduce(
+    (params, current) => {
+      const nitrateDiff = current.nitrate - meanValues.nitrate;
+      const phosphateDiff = current.phosphate - meanValues.phosphate;
+      const calciumDiff = current.calcium - meanValues.calcium;
+      const alkalinityDiff = current.alkalinity - meanValues.alkalinity;
+      params.nitrate += nitrateDiff * nitrateDiff;
+      params.phosphate += phosphateDiff * phosphateDiff;
+      params.calcium += calciumDiff * calciumDiff;
+      params.alkalinity += alkalinityDiff * alkalinityDiff;
+      return params;
+    },
+    { nitrate: 0, phosphate: 0, calcium: 0, alkalinity: 0 }
+  );
+
+  // Calculate the standard deviation using the non-population formula
+  const standardDeviation = {
+    // note: this is where we need 2 or more pieces of data, due to "data.length-1", cannot divide by 0.
+    nitrate: Math.sqrt(squaredDifferences.nitrate / (data.length - 1)),
+    phosphate: Math.sqrt(squaredDifferences.phosphate / (data.length - 1)),
+    calcium: Math.sqrt(squaredDifferences.calcium / (data.length - 1)),
+    alkalinity: Math.sqrt(squaredDifferences.alkalinity / (data.length - 1))
+  };
+  return standardDeviation;
 }
-
-/*
-min, max, and previous?
-
-  alkalinity Float
-  calcium    Float
-  nitrate    Float
-  phosphate  Float
-
-*/
