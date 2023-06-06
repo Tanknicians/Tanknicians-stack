18d433d1bc507cdd3aaf302d21f77595e731a738
SlimyMonz <khyrdantai@icloud.com>
2023-06-06 00:17:39 -0400

    Considering <rootDir> may refer to the actual root rather than the backend root.

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -4,9 +4,9 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   // Jest pathing to match tsconfig.json
   moduleNameMapper: {
-    '^trpc/(.*)$': '<rootDir>/src/trpc/$1',
-    '^prisma/(.*)$': '<rootDir>/prisma/$1',
-    '^types/(.*)$': '<rootDir>/src/types/$1',
+    '^trpc/(.*)$': '<rootDir>/backend/src/trpc/$1',
+    '^prisma/(.*)$': '<rootDir>/backend/prisma/$1',
+    '^types/(.*)$': '<rootDir>/backend/src/types/$1',
   },
 };
 
