5aa242c3c7f342a64765a4dccf1e79a920c16421
SlimyMonz <khyrdantai@icloud.com>
2023-06-07 09:51:54 -0400

    Changed checkServiceCall function to static parameter checking. Put the standard deviation code off to the side for further review and potential refactoring.

--- backend/src/MobileFormService/MobileFormService.ts
+++ backend/src/MobileFormService/MobileFormService.ts
@@ -4,8 +4,8 @@ import { TRPCError } from "@trpc/server";
 
 export async function uploadServiceCall(
   serviceCall: Omit<Prisma.ServiceCall, "id">,
-) {
-  const submitServiceCall = await checkServiceCall(serviceCall);
+): Promise<void> {
+  const submitServiceCall = checkServiceCall(serviceCall);
   try {
     await serviceCallDB.create(submitServiceCall);
   } catch (e) {
@@ -18,57 +18,52 @@ export async function uploadServiceCall(
 }
 
 // run checks on the service call and make sure parameters are valid
-export async function checkServiceCall(
+export function checkServiceCall(
   serviceCall: Omit<Prisma.ServiceCall, "id">,
-) {
-  // No error checking needed if previous do not exist. New customers won't have previous service call forms.
-  const previousServiceCalls = await serviceCallDB.readLatest(
-    serviceCall.tankId,
-  );
-  // need at least 2 service calls to find deltas
-  if (previousServiceCalls.length > 1) {
-    const i = previousServiceCalls.length;
-    const averages = {
-      alkalinity:
-        previousServiceCalls.reduce(
-          (total, serviceCall) => total + serviceCall.alkalinity,
-          0,
-        ) / i,
-      calcium:
-        previousServiceCalls.reduce(
-          (total, serviceCall) => total + serviceCall.calcium,
-          0,
-        ) / i,
-      nitrate:
-        previousServiceCalls.reduce(
-          (total, serviceCall) => total + serviceCall.nitrate,
-          0,
-        ) / i,
-      phosphate:
-        previousServiceCalls.reduce(
-          (total, serviceCall) => total + serviceCall.phosphate,
-          0,
-        ) / i,
-    };
-    // calculate standard deviation of previous service calls
-    const standardDeviation = calculateStandardDeviation(previousServiceCalls);
-    // if there are any deltas that are greater than the standard deviation, flag the service call
-    if (
-      Math.abs(averages.alkalinity - serviceCall.alkalinity) >
-        standardDeviation.alkalinity ||
-      Math.abs(averages.calcium - serviceCall.calcium) >
-        standardDeviation.calcium ||
-      Math.abs(averages.nitrate - serviceCall.nitrate) >
-        standardDeviation.nitrate ||
-      Math.abs(averages.phosphate - serviceCall.phosphate) >
-        standardDeviation.phosphate
-    ) {
-      serviceCall.isApproved = false;
-    }
+): Omit<Prisma.ServiceCall, "id"> {
+
+  const {
+    alkalinity,
+    calcium,
+    nitrate,
+    phosphate
+  } = serviceCall;
+
+  if (
+    alkalinity < paramLimits.alkalinityMin ||
+    alkalinity > paramLimits.alkalinityMax ||
+    calcium < paramLimits.calciumMin ||
+    calcium > paramLimits.calciumMax ||
+    nitrate < paramLimits.nitrateMin ||
+    nitrate > paramLimits.nitrateMax ||
+    phosphate < paramLimits.phosphateMin ||
+    phosphate > paramLimits.phosphateMax
+  ) {
+    serviceCall.isApproved = false;
   }
+
   // return the flagged/unflagged service call
   return serviceCall;
 }
+
+const paramLimits = {
+  /*
+    a.    Calcium at or below 400 and at or above 500 
+    b.    Alkalinity at or below 6.5 and at or above 11
+    c.    Nitrate at or below 1 and at or above 20
+    d.    Phosphate at or below .03 and at or above .24
+  */
+  calciumMin: 400,
+  calciumMax: 500,
+  alkalinityMin: 6.5,
+  alkalinityMax: 11,
+  nitrateMin: 1,
+  nitrateMax: 20,
+  phosphateMin: 0.03,
+  phosphateMax: 0.24
+};
+
+
 
 // Standard deviation calculations, takes in an array of Service Calls without "id"
 function calculateStandardDeviation(data: Omit<Prisma.ServiceCall, "id">[]): {
@@ -120,3 +115,54 @@ function calculateStandardDeviation(data: Omit<Prisma.ServiceCall, "id">[]): {
   };
   return standardDeviation;
 }
+
+
+/* This code used to be part of the MobileFormService "checkServiceCall" code, may be recycled later.
+
+// No error checking needed if previous do not exist. New customers won't have previous service call forms.
+  const previousServiceCalls = await serviceCallDB.readLatest(
+    serviceCall.tankId,
+  );
+  // need at least 2 service calls to find deltas
+  if (previousServiceCalls.length > 1) {
+    const i = previousServiceCalls.length;
+    const averages = {
+      alkalinity:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.alkalinity,
+          0,
+        ) / i,
+      calcium:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.calcium,
+          0,
+        ) / i,
+      nitrate:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.nitrate,
+          0,
+        ) / i,
+      phosphate:
+        previousServiceCalls.reduce(
+          (total, serviceCall) => total + serviceCall.phosphate,
+          0,
+        ) / i,
+    };
+    // calculate standard deviation of previous service calls
+    const standardDeviation = calculateStandardDeviation(previousServiceCalls);
+    // if there are any deltas that are greater than the standard deviation, flag the service call
+    if (
+      Math.abs(averages.alkalinity - serviceCall.alkalinity) >
+        standardDeviation.alkalinity ||
+      Math.abs(averages.calcium - serviceCall.calcium) >
+        standardDeviation.calcium ||
+      Math.abs(averages.nitrate - serviceCall.nitrate) >
+        standardDeviation.nitrate ||
+      Math.abs(averages.phosphate - serviceCall.phosphate) >
+        standardDeviation.phosphate
+    ) {
+      serviceCall.isApproved = false;
+    }
+  }
+
+*/
