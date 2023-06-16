4477e706fbc5c7961326a861f953cdfb7a824b5f
SlimyMonz <khyrdantai@icloud.com>
2023-06-16 16:22:37 -0400

    Removed all occurrences of ZOD.

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,6 +1,5 @@
 import * as Prisma from "@prisma/client";
 import { Request, Response, NextFunction } from "express";
-import { z } from "zod";
 
 // Generic input validation middleware, not currently working? (not implemented either)
 export function validateInput<T extends {}>(type: new () => T) {
@@ -34,63 +33,3 @@ export class RefreshInput {
   email!: string;
   refreshToken!: string;
 }
-
-
-export const Login = z.object({
-  id: z.number().int(),
-  email: z.string().email(),
-  password: z.string(),
-  role: z.nativeEnum(Prisma.Role),
-  userId: z.number().int(),
-});
-
-export const ServiceCall = z.object({
-  id: z.number().int(),
-  isApproved: z.boolean(),
-  createdOn: z.date(),
-  customerRequest: z.string(),
-  employeeNotes: z.string(),
-  alkalinity: z.number(),
-  calcium: z.number(),
-  nitrate: z.number(),
-  phosphate: z.number(),
-  ATOOperational: z.boolean(),
-  ATOReservoirFilled: z.boolean(),
-  chemFilterAdjusted: z.boolean(),
-  doserAdjustementOrManualDosing: z.boolean(),
-  dosingReservoirsFull: z.boolean(),
-  floorsCheckedForSpillsOrDirt: z.boolean(),
-  glassCleanedInside: z.boolean(),
-  glassCleanedOutside: z.boolean(),
-  mechFilterChanged: z.boolean(),
-  notesUpdated: z.boolean(),
-  pumpsClearedOfDebris: z.boolean(),
-  saltCreepCleaned: z.boolean(),
-  skimmerCleanedAndOperational: z.boolean(),
-  waterChanged: z.boolean(),
-  pestAPresent: z.boolean(),
-  pestBPresent: z.boolean(),
-  pestCPresent: z.boolean(),
-  pestDPresent: z.boolean(),
-  employeeId: z.number().int(),
-  tankId: z.number().int(),
-});
-
-export const TankMetadata = z.object({
-  id: z.number().int(),
-  description: z.string().nullable(),
-  volume: z.number().positive(),
-  type: z.nativeEnum(Prisma.TankType),
-  tanknicianSourcedOnly: z.boolean(),
-  lastDateServiced: z.date(),
-  customerId: z.number().int(),
-});
-
-export const User = z.object({
-  id: z.number().int(),
-  firstName: z.string().nullable(),
-  middleName: z.string().nullable(),
-  lastName: z.string().nullable(),
-  address: z.string().nullable(),
-  phone: z.string().nullable(),
-});
