a0c05ef9bebe65b5886b1364b3ed6d877c1750ab
SlimyMonz <khyrdantai@icloud.com>
2023-06-16 16:34:33 -0400

    Unsure if URL needed to be fixed. Will need to test the tests later.

--- backend/src/Authentication/AuthRoutes.test.ts
+++ backend/src/Authentication/AuthRoutes.test.ts
@@ -12,7 +12,7 @@ describe("Authentication API", () => {
     const mockReq = { body: { email: "testemail@gmail.com", password: "hunter2" } };
     const mockRes = { status: jest.fn(), json: jest.fn() };
 
-    await request(app).post("/api/auth/login").send(mockReq.body);
+    await request(app).post("/login").send(mockReq.body);
 
     expect(mockLogin).toHaveBeenCalledWith(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
@@ -26,7 +26,7 @@ describe("Authentication API", () => {
     const mockReq = { body: { username: "testuser", password: "testpassword" } };
     const mockRes = { status: jest.fn(), json: jest.fn() };
 
-    await request(app).post("/api/auth/register").send(mockReq.body);
+    await request(app).post("/register").send(mockReq.body);
 
     expect(mockRegister).toHaveBeenCalledWith(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
@@ -39,7 +39,7 @@ describe("Authentication API", () => {
     const mockReq = { body: { refreshToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJyb2xlIjoiQURNSU4iLCJlbWFpbCI6InRlc3RlbWFpbEBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQxMCRRc3lvWTZOTXBRUjlFQWouS1BkNkIuTkN3ZFl2NUlibDQzZW1CdlRrU3pqZVRLQm8zVnV3aSIsInVzZXJJZCI6MX0sImlzUmVmcmVzaFRva2VuIjp0cnVlLCJpYXQiOjE2ODY5NDY2MjUsImV4cCI6MTY4NzU1MTQyNX0.bu27cOIk3DGdpIQWnqBWWQMw44oq5pKgle5ErxGvp4c" } };
     const mockRes = { status: jest.fn(), json: jest.fn() };
 
-    await request(app).post("/api/auth/refresh").send(mockReq.body);
+    await request(app).post("/refresh").send(mockReq.body);
 
     expect(mockRefresh).toHaveBeenCalledWith(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
