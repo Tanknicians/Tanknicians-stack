75d926b933d3667745bf41a37e0e972a0d72d5de
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2023-08-24 13:52:52 -0400

    added type to endpoint req

--- backend/src/MobileService/MobileRoutes.ts
+++ backend/src/MobileService/MobileRoutes.ts
@@ -7,7 +7,11 @@ import {
   uploadServiceCall,
 } from "./MobileService";
 import { authenticateRoleMiddleWare } from "../Authentication/AuthService";
-import { serviceCallCreateSchema, validateRequestBody } from "../zodTypes";
+import {
+  serviceCallCreateSchema,
+  ServiceCallRequest,
+  validateRequestBody,
+} from "../zodTypes";
 
 const mobileRouter = express.Router();
 mobileRouter.use(express.json());
@@ -17,10 +21,10 @@ mobileRouter.post(
   "/uploadForm",
   authenticateRoleMiddleWare(["ADMIN", "EMPLOYEE"]),
   validateRequestBody(serviceCallCreateSchema),
-  async (req, res) => {
+  async (req: ServiceCallRequest, res) => {
     try {
-      const input = req.body; 
-      const message = await uploadServiceCall(input); // returns a simple "approved/not approved."
+      const input = req.body;
+      const message = await uploadServiceCall(input);
       res.status(200).json({ success: `Form uploaded. Form ${message}.` });
     } catch (error) {
       res.status(500).json({ error: "Failed to upload form." });
