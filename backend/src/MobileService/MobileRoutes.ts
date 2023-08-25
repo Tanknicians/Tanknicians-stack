65cecad2102f1af3c76a03de9d1d6f1c210bdd7a
Harry Hocker <69062179+SlimyMonz@users.noreply.github.com>
2023-08-25 15:43:24 -0400

    Removed unused code for the Mobile API.

--- backend/src/MobileService/MobileRoutes.ts
+++ backend/src/MobileService/MobileRoutes.ts
@@ -1,9 +1,5 @@
 import express from "express";
 import {
-  getAllTanks,
-  getAllUsers,
-  getTanksByUserId,
-  searchUsers,
   uploadServiceCall,
 } from "./MobileService";
 import { authenticateRoleMiddleWare } from "../Authentication/AuthService";
@@ -28,64 +24,6 @@ mobileRouter.post(
       res.status(200).json({ success: `Form uploaded. Form ${message}.` });
     } catch (error) {
       res.status(500).json({ error: "Failed to upload form." });
-    }
-  },
-);
-
-// Sync list of Customers (Custom User model)
-mobileRouter.get(
-  "/allUsers",
-  authenticateRoleMiddleWare(["ADMIN", "EMPLOYEE"]),
-  async (_req, res) => {
-    try {
-      const allUsers = getAllUsers();
-      res.status(200).json(allUsers);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to sync users." });
-    }
-  },
-);
-
-// Sync list of Tanks (Custom TankMetaData model)
-mobileRouter.get(
-  "/allTanks",
-  authenticateRoleMiddleWare(["ADMIN", "EMPLOYEE"]),
-  async (_req, res) => {
-    try {
-      const allTanks = getAllTanks();
-      res.status(200).json(allTanks);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to sync tanks." });
-    }
-  },
-);
-
-// Search DB for Customer (Custom User model)
-mobileRouter.get(
-  "/searchUser",
-  authenticateRoleMiddleWare(["ADMIN", "EMPLOYEE"]),
-  async (req, res) => {
-    try {
-      const searchString = req.body.search;
-      const userSearch = searchUsers(searchString);
-      res.status(200).json(userSearch);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to search for users." });
-    }
-  },
-);
-
-// Get list of tanks (Custom TankMetaData model)
-mobileRouter.get(
-  "/tanks/:id",
-  authenticateRoleMiddleWare(["ADMIN", "EMPLOYEE"]),
-  async (req, res) => {
-    try {
-      const userId = Number(req.params.id);
-      const userTanks = getTanksByUserId(userId);
-      res.status(200).json(userTanks);
-    } catch (error) {
-      res.status(500).json({ error: "Failed to get user's tanks." });
     }
   },
 );
--- backend/src/MobileService/MobileService.ts
+++ backend/src/MobileService/MobileService.ts
@@ -1,7 +1,4 @@
-import * as Prisma from "@prisma/client";
 import { serviceCallDB } from "../../prisma/db/ServiceCall";
-import { userDB } from "../../prisma/db/User";
-import { tankDB } from "../../prisma/db/TankMetadata";
 import { ServiceCall } from "../zodTypes";
 
 export async function uploadServiceCall(serviceCall: ServiceCall) {
@@ -31,7 +28,6 @@ function checkServiceCall(serviceCall: ServiceCall): ServiceCall {
   ) {
     serviceCall.isApproved = false;
   }
-
   // return the flagged/unflagged service call
   return serviceCall;
 }
@@ -52,40 +48,5 @@ const paramLimits = {
   phosphateMin: 0.03,
   phosphateMax: 0.24,
 };
-
-export async function getAllUsers() {
-  try {
-    return await userDB.getAll();
-  } catch (error) {
-    return { error: "Failed to get users." };
-  }
-}
-
-export async function searchUsers(search: string) {
-  if (search === "" || search == null)
-    return { invalid: "search string cannot be empty" };
-  try {
-    return await userDB.searchByString(search);
-  } catch (error) {
-    return { error: "Failed to search users." };
-  }
-}
-
-export async function getAllTanks() {
-  try {
-    return await tankDB.getAll();
-  } catch (error) {
-    return { error: "Failed to get tank list." };
-  }
-}
-
-export async function getTanksByUserId(userId: number) {
-  if (userId < 1) return { invalid: "userId must be a positive integer." };
-  try {
-    return await tankDB.readTanksByUserId(userId);
-  } catch (error) {
-    return { error: "Could not get tanks from user ID" };
-  }
-}
 
 // removed unused code
