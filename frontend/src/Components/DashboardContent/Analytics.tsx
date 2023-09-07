f4e3a87ba7855a8909d82885547c06234433c8ef
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-09-07 18:33:18 -0400

    Added temp styling

--- frontend/src/Components/DashboardContent/Analytics.tsx
+++ frontend/src/Components/DashboardContent/Analytics.tsx
@@ -1,10 +1,17 @@
 import Header from '../DashboardHeader'
 import DefaultCharts from './AnalyticsFunctionality/DefaultCharts'
+
 export default function Analytics(){
+    const style = {
+        height: '100vh'
+    }
+
     return(
         <div>
         <Header selection ='Analytics' />
-        <DefaultCharts/>
+        <div style = {style}>
+            <DefaultCharts/>
+        </div>
     </div>
     )
 }
