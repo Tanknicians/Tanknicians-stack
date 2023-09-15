5ba616b185a47a34ba6774e69d5b2faa38ea9fb5
Justice Smith <justice.smith@abbott.com>
2023-09-15 19:16:42 -0400

    fix lint (non-type) issues with rome

--- frontend/src/Components/Dashboard/DefaultCharts.tsx
+++ frontend/src/Components/Dashboard/DefaultCharts.tsx
@@ -1,32 +1,33 @@
-import ListSubheader from '@mui/material/ListSubheader';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import Collapse from '@mui/material/Collapse';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import { useState } from 'react';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useGetClientsQuery } from '../../Redux/slices/users/userManagementSlice';
-import { useGetTankDataQuery } from '../../Redux/slices/tanks/tankDataSlice';
-import LineChart from '../ChartJS/LineChart';
-import { TankData } from './DUMMYDATA';
+import ListSubheader from "@mui/material/ListSubheader";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useGetClientsQuery } from "../../Redux/slices/users/userManagementSlice";
+import { useGetTankDataQuery } from "../../Redux/slices/tanks/tankDataSlice";
+import LineChart from "../ChartJS/LineChart";
+import { TankData } from "./DUMMYDATA";
+
+interface OwnedTanks {
+  id: number;
+}
+export interface UserData {
+  OwnedTanks: OwnedTanks[];
+  firstName: string;
+  lastName: string;
+  id: number;
+}
 
 export default function DefaultCharts() {
   // interfaces to appease sadistict TS kink enjoyers
-  interface OwnedTanks {
-    id: number;
-  }
-  interface UserData {
-    OwnedTanks: OwnedTanks[];
-    firstName: string;
-    lastName: string;
-    id: number;
-  }
+
   interface UseQuery {
     data: UserData[] | null;
     error: Error | undefined;
@@ -60,7 +61,7 @@ export default function DefaultCharts() {
 
   // on selection of users tankID, generate charts
   const handleRequestChart = (tankID: number) => {
-    console.log('TankID: ' + tankID);
+    console.log(`TankID:·${tankID}`);
     setListToggle(false);
     setToggleShowCharts(true);
   };
@@ -69,12 +70,12 @@ export default function DefaultCharts() {
     return (
       <div>
         <List
-          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-          component='nav'
-          aria-labelledby='nested-list-subheader'
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+          component="nav"
+          aria-labelledby="nested-list-subheader"
           subheader={
-            <ListSubheader component='div' id='nested-list-subheader'>
-              Clients{' '}
+            <ListSubheader component="div" id="nested-list-subheader">
+              Clients{" "}
               <IconButton onClick={handleListToggle}>
                 <MenuIcon />
               </IconButton>
@@ -85,19 +86,19 @@ export default function DefaultCharts() {
             {myUserData?.map(({ id, firstName, lastName, OwnedTanks }) => (
               <div>
                 <ListItemButton onClick={() => toggleSubMenu(id)} key={id}>
-                  <ListItemText primary={firstName + ' ' + lastName} />
+                  <ListItemText primary={`${firstName}·${lastName}`} />
                   {id === openItemId ? <ExpandLess /> : <ExpandMore />}
                 </ListItemButton>
 
-                <Collapse in={id === openItemId} timeout='auto' unmountOnExit>
-                  <List component='div' disablePadding>
+                <Collapse in={id === openItemId} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
                     {OwnedTanks.map(({ id }) => (
                       <ListItemButton
                         sx={{ pl: 4 }}
                         onClick={() => handleRequestChart(id)}
                         key={id}
                       >
-                        <ListItemText primary={'Select Tank ' + id} />
+                        <ListItemText primary={`Select·Tank·${id}`} />
                       </ListItemButton>
                     ))}
                   </List>
@@ -108,15 +109,15 @@ export default function DefaultCharts() {
         </List>
         <h2>One Chart Demo</h2>
         <div
-          style={{ alignContent: 'center', width: '50%', position: 'relative' }}
+          style={{ alignContent: "center", width: "50%", position: "relative" }}
         >
           {toggleShowCharts &&
             TankData.map((d) => (
               <div>
                 <h3>{d.datasets[0].label}</h3>
                 <Card
-                  variant='outlined'
-                  sx={{ maxWidth: '80%', minWidth: '50%' }}
+                  variant="outlined"
+                  sx={{ maxWidth: "80%", minWidth: "50%" }}
                 >
                   <CardContent>
                     <LineChart data={d} />
--- frontend/src/Redux/slices/user/userTanksApiSlice.ts
+++ frontend/src/Redux/slices/user/userTanksApiSlice.ts
@@ -4,7 +4,7 @@ export const userTankApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     userWithTanks: builder.query({
       query: (includeTanks) => ({
-        url: `api/database/user`,
+        url: 'api/database/user',
         method: 'GET',
         params: {
           includeTanks
