2d43410a799dcd86ac06c48abd76eaa86a88caa0
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2023-10-08 19:34:46 -0400

    changed getbyid to refs

--- frontend/src/components/QRCodeCard.tsx
+++ frontend/src/components/QRCodeCard.tsx
@@ -2,13 +2,10 @@ import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import { UserData } from '../redux/slices/users/userManagementSlice';
 import { Button, Stack, Typography } from '@mui/material';
 import QRCodeCanvas from 'qrcode.react';
+import { useRef } from 'react';
 
 function QRValue(tankId: number) {
   return `Tanknicians Tank ID: ${tankId}`;
-}
-
-function IDValue(tankId: number, qrSymbol: number) {
-  return `${tankId}${qrSymbol}`;
 }
 
 function QRCodeFile(client: UserData, qrSymbol: number) {
@@ -19,12 +16,16 @@ export default function QRCodeCard({
   client,
   tankId,
   qrSymbol
-}: { client: UserData; tankId: number; qrSymbol: number }) {
+}: {
+  client: UserData;
+  tankId: number;
+  qrSymbol: number;
+}) {
+  const qrCodeRef = useRef<HTMLDivElement>(null);
+
   const downloadQRCode = () => {
     // Generate download with use canvas and stream
-    const canvas = document.getElementById(
-      IDValue(tankId, qrSymbol)
-    ) as HTMLCanvasElement;
+    const canvas = qrCodeRef.current?.children[0] as HTMLCanvasElement;
     if (canvas) {
       const pngUrl = canvas
         .toDataURL('image/png')
@@ -40,19 +41,21 @@ export default function QRCodeCard({
 
   return (
     <Stack justifyContent='center' alignItems='center'>
-      <QRCodeCanvas
-        id={IDValue(tankId, qrSymbol)}
-        value={QRValue(tankId)}
-        level='H'
-        size={125}
-        includeMargin={true}
-      />
+      <div ref={qrCodeRef}>
+        <QRCodeCanvas
+          value={QRValue(tankId)}
+          level='H'
+          size={125}
+          includeMargin={true}
+        />
+      </div>
       <Typography variant='body1' gutterBottom>
         Tank {qrSymbol}
       </Typography>
       <Button
         variant='contained'
         onClick={downloadQRCode}
+        size='small'
         endIcon={<FileDownloadOutlinedIcon />}
       >
         Download QR Code
