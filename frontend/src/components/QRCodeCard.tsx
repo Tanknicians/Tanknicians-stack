7c9bacf6519f099e68bec7bded1ce1b6aa1206b3
Carlos Martinez <carlosmartinezagv@gmail.com>
2023-10-09 13:57:19 -0400

    added tank number to png

--- frontend/src/components/QRCodeCard.tsx
+++ frontend/src/components/QRCodeCard.tsx
@@ -1,8 +1,8 @@
-import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
-import { UserData } from '../redux/slices/users/userManagementSlice';
-import { Button, Stack, Typography } from '@mui/material';
-import QRCodeCanvas from 'qrcode.react';
-import { useRef } from 'react';
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+import { UserData } from "../redux/slices/users/userManagementSlice";
+import { Button, Stack, Typography } from "@mui/material";
+import QRCodeCanvas from "qrcode.react";
+import { useRef } from "react";
 
 function QRValue(tankId: number) {
   return `Tanknicians Tank ID: ${tankId}`;
@@ -12,10 +12,47 @@ function QRCodeFile(client: UserData, qrSymbol: number) {
   return `${client.firstName}-${client.lastName}-Tank${qrSymbol}QRCode.png`;
 }
 
+function QRCodeTextCanvas(qrCanvas: HTMLCanvasElement, qrSymbol: number) {
+  // Create a new canvas for combining QR code and text
+  const combinedCanvas = document.createElement("canvas");
+  const combinedContext = combinedCanvas.getContext("2d");
+
+  if (qrCanvas && combinedCanvas && combinedContext) {
+    const text = `Tank ${qrSymbol}`;
+    const fontSize = 16;
+    const lineHeight = 20;
+    const canvasWidth = qrCanvas.width;
+    const canvasHeight = qrCanvas.height + lineHeight;
+
+    combinedCanvas.width = canvasWidth;
+    combinedCanvas.height = canvasHeight;
+
+    // Draw QR code
+    combinedContext.drawImage(qrCanvas, 0, 0);
+
+    // Draw text background
+    combinedContext.fillStyle = "white";
+    combinedContext.fillRect(0, qrCanvas.height, canvasWidth, lineHeight);
+
+    // Draw text and offset it vertically
+    const offset = 10;
+    combinedContext.font = `bold ${fontSize}px sans-serif`;
+    combinedContext.fillStyle = "black";
+    combinedContext.textAlign = "center";
+    combinedContext.fillText(
+      text,
+      canvasWidth / 2,
+      qrCanvas.height + lineHeight - offset,
+    );
+    return combinedCanvas;
+  }
+  return null;
+}
+
 export default function QRCodeCard({
   client,
   tankId,
-  qrSymbol
+  qrSymbol,
 }: {
   client: UserData;
   tankId: number;
@@ -24,13 +61,17 @@ export default function QRCodeCard({
   const qrCodeRef = useRef<HTMLDivElement>(null);
 
   const downloadQRCode = () => {
-    // Generate download with use canvas and stream
-    const canvas = qrCodeRef.current?.children[0] as HTMLCanvasElement;
-    if (canvas) {
-      const pngUrl = canvas
-        .toDataURL('image/png')
-        .replace('image/png', 'image/octet-stream');
-      const downloadLink = document.createElement('a');
+    const qrCanvas = qrCodeRef.current?.children[0] as HTMLCanvasElement;
+    const combinedCanvas = QRCodeTextCanvas(qrCanvas, qrSymbol);
+
+    if (combinedCanvas) {
+      // Convert canvas to PNG data URL
+      const pngUrl = combinedCanvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+
+      // Trigger download
+      const downloadLink = document.createElement("a");
       downloadLink.href = pngUrl;
       downloadLink.download = QRCodeFile(client, qrSymbol);
       document.body.appendChild(downloadLink);
@@ -40,22 +81,22 @@ export default function QRCodeCard({
   };
 
   return (
-    <Stack justifyContent='center' alignItems='center'>
+    <Stack justifyContent="center" alignItems="center">
       <div ref={qrCodeRef}>
         <QRCodeCanvas
           value={QRValue(tankId)}
-          level='H'
+          level="H"
           size={125}
           includeMargin={true}
         />
+        <Typography textAlign="center" variant="body1" gutterBottom>
+          Tank {qrSymbol}
+        </Typography>
       </div>
-      <Typography variant='body1' gutterBottom>
-        Tank {qrSymbol}
-      </Typography>
       <Button
-        variant='contained'
+        variant="contained"
         onClick={downloadQRCode}
-        size='small'
+        size="small"
         endIcon={<FileDownloadOutlinedIcon />}
       >
         Download QR Code
