b01bea17e1bd147e2568d5e453d48adb3d3f2757
Carlos Martinez <carlosmartinezagv@gmail.com>
2023-10-14 21:48:58 -0400

    fix for edit button responsive layout

--- frontend/src/components/UserCard.tsx
+++ frontend/src/components/UserCard.tsx
@@ -1,8 +1,6 @@
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
 import { UserData } from '../redux/slices/users/userManagementSlice';
 import { useState } from 'react';
 import EditUserModal from './forms/EditUser';
@@ -33,74 +31,65 @@ export default function UserCard(props: UserCardProps) {
     setAnchorEl(null);
   };
 
+  if (!user) return null;
+
   return (
     <>
       <Paper elevation={3}>
-        <Grid container maxWidth={'100%'}>
-          {user && (
-            <>
-              <Grid item xs={10} md={3}>
-                <Typography
-                  sx={{ padding: 1 }}
-                  variant='subtitle1'
-                  component='h2'
-                >
-                  {`${user.firstName} ${user.middleName} ${user.lastName}`}
-                </Typography>
-              </Grid>
-              <Grid item xs={10} md={4}>
-                <Typography
-                  sx={{ padding: 1 }}
-                  variant='subtitle1'
-                  component='h2'
-                >
-                  {user.address}
-                </Typography>
-              </Grid>
-              <Grid item xs={10} md={3}>
-                <Typography
-                  sx={{ padding: 1 }}
-                  variant='subtitle1'
-                  component='h2'
-                >
-                  {user.phone}
-                </Typography>
-              </Grid>
-              <Grid
-                item
-                xs={2}
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  height: '100%'
-                }}
-              >
-                <IconButton
-                  aria-label='more'
-                  id='long-button'
-                  aria-controls={open ? 'long-menu' : undefined}
-                  aria-expanded={open ? 'true' : undefined}
-                  aria-haspopup='true'
-                  onClick={handleClick}
-                >
-                  <MoreVertIcon />
-                </IconButton>
-                <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-                  <MenuItem onClick={handleOpenUserModal}>Edit</MenuItem>
-                </Menu>
-              </Grid>
-            </>
-          )}
+        <Grid
+          container
+          maxWidth={'100%'}
+          padding={1}
+          alignItems={{ lg: 'center' }}
+        >
+          <Grid container xs={10}>
+            <Grid item xs={12} md={4}>
+              <Typography padding={1} variant='subtitle1' component='h2'>
+                {`${user.firstName} ${user.middleName} ${user.lastName}`}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Typography padding={1} variant='subtitle1' component='h2'>
+                {user.address}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Typography padding={1} variant='subtitle1' component='h2'>
+                {user.phone}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'start'
+            }}
+          >
+            <IconButton
+              aria-label='more'
+              id='long-button'
+              aria-controls={open ? 'long-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-haspopup='true'
+              onClick={handleClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+            <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+              <MenuItem onClick={handleOpenUserModal}>Edit</MenuItem>
+            </Menu>
+          </Grid>
         </Grid>
       </Paper>
-      {userModalOpen && (
-        <EditUserModal
-          open={userModalOpen}
-          setOpen={setUserModalOpen}
-          userData={user}
-          key={user?.id}
-        />
-      )}
+      <EditUserModal
+        open={userModalOpen}
+        setOpen={setUserModalOpen}
+        userData={user}
+        key={user?.id}
+      />
     </>
   );
 }
