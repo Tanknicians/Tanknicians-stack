f5df1a9b2904b8ee31637ea65cc195486ec3fe0f
Carlos Martinez <carlosmartinezagv@gmail.com>
2023-10-16 01:31:31 -0400

    refetching DG SC works unapproved forms does not

--- frontend/src/redux/slices/forms/servicecallApiSlice.ts
+++ frontend/src/redux/slices/forms/servicecallApiSlice.ts
@@ -4,21 +4,38 @@ import { apiSlice } from '../../api/apiSlice';
 export const servicecallApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getAllServiceCalls: builder.query<ServiceCall[], { isApproved?: boolean }>({
-      query: () => ({ url: '/api/database/servicecall', method: 'GET' })
+      query: () => ({ url: '/api/database/servicecall', method: 'GET' }),
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({
+                type: 'SERVICECALL' as const,
+                id
+              })),
+              { type: 'SERVICECALL', id: 'LIST' }
+            ]
+          : [{ type: 'SERVICECALL', id: 'LIST' }]
     }),
     createServiceCall: builder.mutation({
       query: (serviceCall) => ({
         url: '/api/database/servicecall',
         method: 'POST',
         body: { ...serviceCall }
-      })
+      }),
+      invalidatesTags: () => [{ type: 'SERVICECALL', id: 'LIST' }]
     }),
     updateServiceCall: builder.mutation({
       query: ({ id, ...serviceCall }) => ({
         url: `/api/database/servicecall/${id}`,
         method: 'PUT',
         body: serviceCall
-      })
+      }),
+      invalidatesTags: ({ id }) => {
+        return [
+          { type: 'SERVICECALL', id },
+          { type: 'UNAPPROVEDSERVICECALL', id: 'LIST' }
+        ];
+      }
     }),
     getServiceCallByTankId: builder.query<
       ServiceCall[],
@@ -28,14 +45,24 @@ export const servicecallApiSlice = apiSlice.injectEndpoints({
         url: `/api/database/servicecall/fromTank/${tankId}`,
         method: 'GET',
         params: { isApproved: isApproved }
-      })
+      }),
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({
+                type: 'SERVICECALL' as const,
+                id
+              }))
+            ]
+          : [{ type: 'SERVICECALL', id: 'LIST' }]
     }),
     getUnapprovedServiceCalls: builder.query<ServiceCall[], void>({
       query: () => ({
         url: '/api/database/servicecall',
         method: 'GET',
         params: { isApproved: false }
-      })
+      }),
+      providesTags: () => [{ type: 'UNAPPROVEDSERVICECALL', id: 'LIST' }]
     })
   })
 });
