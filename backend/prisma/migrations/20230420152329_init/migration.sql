2f742447e3ec012310ecb9fc45b9af08a9612ba5
Harry Hocker <69062179+SlimyMonz@users.noreply.github.com>
2023-10-18 19:12:42 -0400

    Forgot a comma. Facepalm.

--- backend/prisma/migrations/20230420152329_init/migration.sql
+++ backend/prisma/migrations/20230420152329_init/migration.sql
@@ -18,8 +18,8 @@ CREATE TABLE `User` (
     `lastName` VARCHAR(191) NOT NULL DEFAULT '',
     `address` VARCHAR(191) NOT NULL DEFAULT '',
     `phone` VARCHAR(191) NOT NULL DEFAULT '',
-    `email` VARCHAR(191) NOT NULL DEFAULT ''
-    `isEmployee` BOOLEAN NOT NULL DEFAULT 0,
+    `email` VARCHAR(191) NOT NULL DEFAULT '',
+    `isEmployee` BOOLEAN NOT NULL DEFAULT false,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
