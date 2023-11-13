0d0c779b7db95b8201d238f4fe5f5142b68110d3
Will Mitchell <53200009+wmitch34@users.noreply.github.com>
2023-11-13 16:11:02 -0500

    Timezone fixes and chart data update

--- frontend/src/redux/slices/forms/servicecallApiSlice.ts
+++ frontend/src/redux/slices/forms/servicecallApiSlice.ts
@@ -1,5 +1,5 @@
-import { CreateServiceCall, ServiceCall } from '../../../zodTypes';
-import { apiSlice } from '../../api/apiSlice';
+import { CreateServiceCall, ServiceCall } from "../../../zodTypes";
+import { apiSlice } from "../../api/apiSlice";
 
 interface ReturnDataSchema {
   tankId: number;
@@ -12,17 +12,17 @@ interface ReturnDataSchema {
 export const servicecallApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getAllServiceCalls: builder.query<ServiceCall[], { isApproved?: boolean }>({
-      query: () => ({ url: '/api/database/servicecall', method: 'GET' }),
+      query: () => ({ url: "/api/database/servicecall", method: "GET" }),
       providesTags: (result) =>
         result
           ? [
               ...result.map(({ id }) => ({
-                type: 'SERVICECALL' as const,
-                id
+                type: "SERVICECALL" as const,
+                id,
               })),
-              { type: 'SERVICECALL', id: 'LIST' }
+              { type: "SERVICECALL", id: "LIST" },
             ]
-          : [{ type: 'SERVICECALL', id: 'LIST' }]
+          : [{ type: "SERVICECALL", id: "LIST" }],
     }),
     getAllServiceCallsByTankIdAndDateRange: builder.query<
       ReturnDataSchema,
@@ -30,31 +30,33 @@ export const servicecallApiSlice = apiSlice.injectEndpoints({
     >({
       query: ({ tankId, start, end }) => ({
         url: `/api/database/servicecall/range/${tankId}`,
-        method: 'GET',
-        params: { start, end }
-      })
+        method: "GET",
+        params: { start, end },
+      }),
+      providesTags: (result) => [{ type: "SERVICECALL", id: "LIST" }],
     }),
     createServiceCall: builder.mutation<void, CreateServiceCall>({
       query: (serviceCall) => ({
-        url: '/api/database/servicecall',
-        method: 'POST',
-        body: { ...serviceCall }
+        url: "/api/database/servicecall",
+        method: "POST",
+        body: { ...serviceCall },
       }),
-      invalidatesTags: () => [{ type: 'SERVICECALL', id: 'LIST' }]
+      invalidatesTags: () => [{ type: "SERVICECALL", id: "LIST" }],
     }),
     updateServiceCall: builder.mutation<void, ServiceCall>({
       query: ({ id, ...serviceCall }) => ({
         url: `/api/database/servicecall/${id}`,
-        method: 'PUT',
-        body: serviceCall
+        method: "PUT",
+        body: serviceCall,
       }),
       invalidatesTags: (_result, _error, serviceCall) => {
         return [
-          { type: 'SERVICECALL', id: serviceCall.id },
-          { type: 'UNAPPROVEDSERVICECALL', id: serviceCall.id },
-          { type: 'TANKS', id: serviceCall.tankId }
+          { type: "SERVICECALL", id: serviceCall.id },
+          { type: "UNAPPROVEDSERVICECALL", id: serviceCall.id },
+          { type: "TANKS", id: serviceCall.tankId },
+          { type: "SERVICECALL", id: "LIST" },
         ];
-      }
+      },
     }),
     getServiceCallByTankId: builder.query<
       ServiceCall[],
@@ -62,38 +64,38 @@ export const servicecallApiSlice = apiSlice.injectEndpoints({
     >({
       query: ({ tankId, isApproved }) => ({
         url: `/api/database/servicecall/fromTank/${tankId}`,
-        method: 'GET',
-        params: { isApproved: isApproved }
+        method: "GET",
+        params: { isApproved: isApproved },
       }),
       providesTags: (result) =>
         result
           ? [
               ...result.map(({ id }) => ({
-                type: 'SERVICECALL' as const,
-                id
+                type: "SERVICECALL" as const,
+                id,
               })),
-              { type: 'SERVICECALL', id: 'LIST' }
+              { type: "SERVICECALL", id: "LIST" },
             ]
-          : [{ type: 'SERVICECALL', id: 'LIST' }]
+          : [{ type: "SERVICECALL", id: "LIST" }],
     }),
     getUnapprovedServiceCalls: builder.query<ServiceCall[], void>({
       query: () => ({
-        url: '/api/database/servicecall',
-        method: 'GET',
-        params: { isApproved: false }
+        url: "/api/database/servicecall",
+        method: "GET",
+        params: { isApproved: false },
       }),
       providesTags: (result) =>
         result
           ? [
               ...result.map(({ id }) => ({
-                type: 'UNAPPROVEDSERVICECALL' as const,
-                id
+                type: "UNAPPROVEDSERVICECALL" as const,
+                id,
               })),
-              { type: 'UNAPPROVEDSERVICECALL', id: 'LIST' }
+              { type: "UNAPPROVEDSERVICECALL", id: "LIST" },
             ]
-          : [{ type: 'UNAPPROVEDSERVICECALL', id: 'LIST' }]
-    })
-  })
+          : [{ type: "UNAPPROVEDSERVICECALL", id: "LIST" }],
+    }),
+  }),
 });
 
 export const {
@@ -102,5 +104,5 @@ export const {
   useCreateServiceCallMutation,
   useUpdateServiceCallMutation,
   useGetUnapprovedServiceCallsQuery,
-  useGetAllServiceCallsByTankIdAndDateRangeQuery
+  useGetAllServiceCallsByTankIdAndDateRangeQuery,
 } = servicecallApiSlice;
